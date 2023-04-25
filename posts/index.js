532bc86bab3b9ad14f1641afa8f9ccca3cac223f
Lotz Christoph <christoph.lotz@iic-solutions.com>
2023-04-25 14:41:42 +0200

    update

--- posts/index.js
+++ posts/index.js
@@ -1,10 +1,12 @@
 const express = require('express');
-const { randomBytes, scryptSync } = require('crypto');
 const bodyParser = require('body-parser');
+const { randomBytes } = require('crypto');
+const cors = require('cors');
+const axios = require('axios');
 
 const app = express();
-
 app.use(bodyParser.json());
+app.use(cors());
 
 const posts = {};
 
@@ -12,7 +14,7 @@ app.get('/posts', (req, res) => {
   res.send(posts);
 });
 
-app.post('/posts', (req, res) => {
+app.post('/posts', async (req, res) => {
   const id = randomBytes(4).toString('hex');
   const { title } = req.body;
 
@@ -21,9 +23,26 @@ app.post('/posts', (req, res) => {
     title,
   };
 
+  try {
+    await axios.post('http://localhost:4005/events', {
+      type: 'PostCreated',
+      data: {
+        id,
+        title,
+      },
+    });
+  } catch (err) {
+    console.log('An error occured');
+  }
+
   res.status(201).send(posts[id]);
 });
 
+app.post('/events', async (req, res) => {
+  console.log('Received event', req.body.type);
+  res.send({});
+});
+
 app.listen(4000, () => {
-  console.log('Posts listening on 4000');
+  console.log('Posts istening on 4000');
 });
